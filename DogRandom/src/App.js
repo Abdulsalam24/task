5ac54d51fb5725bd32dcd5757ac707e4dca4bbc2
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-11-20 12:32:41 +0100

    three project

---
+++ DogRandom/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import DogRandom from "./page/DogRandom";
+
+
+const App = () => {
+  return (
+    <div className="center">
+      <DogRandom/>
+    </div>
+  )
+}
+
+export default App
---
+++ photoGallery/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import PhotoGallery from "./page/PhotoGallery";
+
+const App = () => {
+  return (
+    <div className="center">
+      <PhotoGallery />
+    </div>
+  )
+}
+
+export default App
