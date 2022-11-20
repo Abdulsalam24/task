c2fc3fced141ac7cc24ae35a9379028153ecf2b1
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-11-20 12:40:36 +0100

    commit

---
+++ timerChange/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Timer from "./page/Timer";
+
+const App = () => {
+  return (
+    <div className="center">
+      <Timer />
+    </div>
+  )
+}
+
+export default App
---
+++ timerChange/src/page/Timer.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+
+import '../asset/timerstyle.css'
+
+const Timer = () => {
+
+    const [input, setInput] = useState('')
+    const [timer, setTimer] = useState(5)
+
+    let timeout
+
+    useEffect(() => {
+        timeout = setTimeout(() => {
+            if (timer > 0) {
+                setTimer((c) => c - 1)
+            } else if (timer < 0) {
+                setTimer((c) => ++c)
+            } else if (timer === 0) {
+                setTimer(0)
+            }
+        }, 1000)
+        return () => {
+            clearInterval(timeout)
+        }
+    }, [timer])
+
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handlePlay = () => {
+        if (input !== "") {
+            setTimer(input)
+        }
+    }
+
+    const handlePause = () => {
+        clearInterval(timeout)
+    }
+
+    const handleResume = () => {
+        setTimeout(() => {
+            if (timer > 0) {
+                setTimer((c) => c - 1)
+            } else if (timer < 0) {
+                setTimer((c) => ++c)
+            } else if (timer === 0) {
+                setTimer(0)
+            }
+        }, 1000)
+    }
+
+    const handleReset = () => {
+        setTimer(0)
+        setInput("")
+    }
+
+    return (
+        <div className='timer'>Timer : {timer}
+            <input type="number" value={input} placeholder='user input' onChange={handleChange} />
+            <div>
+                <button onClick={handlePlay}>play</button>
+                <button onClick={handlePause}>pause</button>
+                <button onClick={handleResume}>resume</button>
+                <button onClick={handleReset}>reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default Timer
